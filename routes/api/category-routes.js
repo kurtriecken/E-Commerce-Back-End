b3d2cab4cea0c98d90d68f5e54f0857563d41150
Kurt Riecken <kurt.riecken@gmail.com>
2023-11-02 18:57:18 -0400

    created CRUD routes for category and tag

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,7 +11,7 @@ router.get('/', async (req, res) => {
     });
 
     if (!categoryData) {
-      res.status(404).json(({ message: "No catgeory data found! Did you seed?" }));
+      res.status(404).json({ message: "No catgeory data found! Did you seed?" });
     };
 
     res.status(200).json(categoryData);
@@ -51,8 +51,23 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    });
+    
+    if (!categoryData) {
+      res.status(404).json(({ message: "No catgeory data found for that ID" }));
+    };
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
